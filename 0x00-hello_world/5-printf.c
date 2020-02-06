7dfd34f685c2fe35c9d8e170b159df54cb1b884d
mfcrespo <mafe_crespo@hotmail.com>
2020-02-06 03:03:40 +0000

    Add task 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Use the function printf
+ *
+ *Description: This function will print the string "with proper grammar,
+ *but the outcome is a piece of art, using the printf function.
+ *Return: The program should end with the value 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
