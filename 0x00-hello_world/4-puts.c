f721f6bd1bad40f9472747070ab241c078f7fba9
mfcrespo <mafe_crespo@hotmail.com>
2020-02-06 02:18:39 +0000

    Add task 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
+
 /**
- *Description: This function will print the string "Programming is like
- *building a multilingual puzzle using the puts function.
- *The program should end with the value 0
- */
+ *main - Use the function puts
+ *
+*Description: This function will print the string "Programming is like
+*building a multilingual puzzle using the puts function.
+*Return: The program should end with the value 0
+*/
+
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
 
