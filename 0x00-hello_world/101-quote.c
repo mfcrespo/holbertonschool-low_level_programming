e1bc3fc3258fc15a0e184eefd284aa5e1804a5cb
mfcrespo <mafe_crespo@hotmail.com>
2020-02-06 15:55:10 +0000

    Add task 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
-#include<stdlib.h>
+#include<unistd.h>
 
 /**
- *main: write a program whithout printf and puts
+ *main - write whithout printf and puts
+ *
  *Description: Write a C program that prints exactly
  *and that piece of art is useful" - Dora Korpar,
  *2015-10-19, followed by a new line, to the standard error
+ *Return is 1
  */
 
-main (void)
+int main(void)
 {
-  write("and that piece of art is useful"" - Dora Korpar, 2015-10-19\n");
-  return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
