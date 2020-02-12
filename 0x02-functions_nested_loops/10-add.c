49771336565ddfd3130a43476d1c0b287f472c0d
mfcrespo <mafe_crespo@hotmail.com>
2020-02-12 01:32:10 +0000

    Add task 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *add - Adds two integers and returns the result
+ *@n1: variable 1
+ *@n2: variable 2
+ *Return: 0
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
