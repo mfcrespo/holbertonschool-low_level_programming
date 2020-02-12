741fb93bca8fddd0a964a39ab68b342f30f281ca
mfcrespo <mafe_crespo@hotmail.com>
2020-02-12 02:44:55 +0000

    Add positive_or_negtive

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,30 +1,25 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-
 /**
- *main - will assign a random number to the variable n each time it
- *is executed. Complete the source code in order to print whether
- *the number stored in the variable n is positive or negative.
+ * main - Prints according to condition
  *
- *Return: if the number is greater than 0: is positive
- *if the number is 0: is zero
- *if the number is less than 0: is negative
+ * Return: Always 0
  */
-
-void positive_or_negative(int n)
+void positive_or_negative(int i)
 {
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
 	else
-		printf("%d is zero\n", n);
+	{
+		printf("%d is negative\n", i);
+	}
 
-	return (0);
 }
