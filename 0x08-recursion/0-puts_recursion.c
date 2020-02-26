4f0ec168ba7fac4bfb9325a91fabcf7c53e309f9
mfcrespo <mafe_crespo@hotmail.com>
2020-02-26 20:10:41 +0000

    Add task 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,12 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
