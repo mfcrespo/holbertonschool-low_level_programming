53366e9023ecca1b99a051c38c8aa1b921fe2161
mfcrespo <mafe_crespo@hotmail.com>
2020-03-02 04:40:48 +0000

    Add task 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#Include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Write a program that adds positive numbers
+ *@argc: number of command line arguments
+ *@argv: array of pointers to argument strings
+ *
+ *Description: Adds positive numbers
+ *Return: 0 when successful, 1 when there are no digits
+ */
+int main(int argc, char *argv[])
+{
+	int add = 0, co = 1;
+	while (co < argc)
+	{
+		int co1 = 0;
+
+		add = add + atoi(argv[co]);
+
+		while (argv[co][co1] != '\0')
+		{
+			if (!(isdigit(argv[co][co1])) || atoi(argv[co]) < 1)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			co1++;
+		}
+		co++;
+	}
+
+	printf("%d\n", add);
+
+	return (0);
+}
