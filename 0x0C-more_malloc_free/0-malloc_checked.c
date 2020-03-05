125cc97a915fe31fb2a8d834be2338d3b869db91
mfcrespo <mafe_crespo@hotmail.com>
2020-03-05 19:42:26 +0000

    Add task 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *malloc_checked - function that allocates memory using malloc
+ *if malloc fails, the malloc_checked function should cause
+ *normal process termination with a status value of 98
+ *@b: pointer
+ *Return: success p - Error 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+if (p == NULL)
+	exit(98);
+	else
+		return (p);
+}
