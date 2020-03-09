0d460ed5f759cf0a81c37badacd780724ae60d8f
mfcrespo <mafe_crespo@hotmail.com>
2020-03-09 23:07:43 +0000

    Add task dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - Define a new type struct dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
