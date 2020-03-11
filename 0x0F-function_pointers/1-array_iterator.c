3598b8bf5aba9a6afc43cb1ba0cda0836c72b752
mfcrespo <mafe_crespo@hotmail.com>
2020-03-11 21:10:36 +0000

    Add typo

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -16,5 +16,5 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || action == NULL)
 		return;
 	for (; i < size; i++)
-		(*action)(*(array + 1));
+		(*action)(*(array + i));
 }
