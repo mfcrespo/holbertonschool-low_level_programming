0be34a7611e02c06f3034645e3906a88c1696889
mfcrespo <mafe_crespo@hotmail.com>
2020-03-20 03:59:07 +0000

    Add lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,21 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/**
+ * struct list - singly linked list
+ * @str: string
+ * @len: length of the string
+ * @nextnode: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_t
+{
+	char *str;
+	unsigned int len;
+	struct list_t *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
