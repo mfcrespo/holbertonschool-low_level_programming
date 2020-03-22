6b18e7dfc9ef7f9ad7ab0e09a010da1155979ef3
mfcrespo <mafe_crespo@hotmail.com>
2020-03-22 03:04:24 +0000

    Add lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,11 +3,12 @@
 
 /**
  * struct list_t - singly linked list
- * @str: string
+ * @str: string (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ *for Holberton project
  */
 typedef struct list_t
 {
@@ -21,5 +22,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
+void printphrase(void) __attribute__((constructor));
 #endif
