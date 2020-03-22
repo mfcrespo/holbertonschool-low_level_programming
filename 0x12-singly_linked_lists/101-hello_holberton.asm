8419b3cc1e34a72d54d3b26764bc1f2b74aa1c5c
mfcrespo <mafe_crespo@hotmail.com>
2020-03-22 03:18:37 +0000

    add task 101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .data
+calypso:	 db "Hello, Holberton", 10,0
+
+	section .text
+	    global main
+
+main:
+	    mov     rax, 1
+	    mov     rdi, 1
+	    mov     rsi, calypso
+	    mov     rdx, 17
+	    syscall
+	;; return (0)
+	    mov     rax, 60
+	    mov     rdi, 0
+	    syscall
