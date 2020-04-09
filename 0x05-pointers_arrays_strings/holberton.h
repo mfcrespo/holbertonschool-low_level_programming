7703540086e0c4e719f4c0b85d1647996566d74e
mfcrespo <mafe_crespo@hotmail.com>
2020-04-09 00:47:50 +0000

    New prototype

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,9 +1,10 @@
 #ifndef HOLBERTON_H
-#define HOLNERTON_H
+#define HOLBERTON_H
 
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+int _atoi(char *s);
 
 #endif
