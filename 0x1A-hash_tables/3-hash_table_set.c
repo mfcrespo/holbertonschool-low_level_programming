ff2e5896ee79301523bc41c27ab5f4f13aca8f5f
mfcrespo <mafe_crespo@hotmail.com>
2020-05-30 02:10:36 +0000

    Add task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+/**
+ *hash_table_set - adds an element to the hash table
+ *@ht: the hash table to add or update to
+ *@key: the key string
+ *@value: the value associated with the key
+ *
+ *Return: 1 if successful, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new;
+	hash_node_t *temp;
+
+	if (ht == NULL)
+		return (0);
+	if (key == NULL || key[0] == '\0')
+		return (0);
+	if (ht->size == 0 || ht->array == NULL)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	temp = ht->array[index];
+
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			return (1);
+		}
+		temp = temp->next;
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+
+	new->key = strdup(key);
+	new->value = strdup(value);
+
+	if (ht->array[index] != NULL)
+		new->next = ht->array[index];
+	else
+		new->next = NULL;
+
+	ht->array[index] = new;
+	return (1);
+}
