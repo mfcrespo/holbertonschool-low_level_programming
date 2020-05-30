645d0845efbab271a04c0fe3fe6ddbe32af0450d
mfcrespo <mafe_crespo@hotmail.com>
2020-05-30 03:47:20 +0000

    Add task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+/**
+ *hash_table_get - Write a function that retrieves a value associated
+ *with a key
+ *@ht: is the hash table you want to look into
+ *@key: is the key you are looking for
+ *
+ *Return: the value associated with the element, or NULL if
+ *key couldn’t be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return (NULL);
+
+	if (key[0] == '\0' || key == NULL)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
+	if (node == NULL)
+		return (NULL);
+
+/*Check if there is a linked list entry*/
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+	return (NULL);
+}
