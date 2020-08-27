aedee0d1466ab0871b29e5c1e1b22d1b05eddb09
mfcrespo <mafe_crespo@hotmail.com>
2020-08-27 23:33:15 +0000

    Includes prototype jump_search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,6 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t head, size_t end);
 int binary(int *array, size_t sub_head, size_t sub_end, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif
